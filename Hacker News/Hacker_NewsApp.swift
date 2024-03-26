9174753eea2648b6e96c7adf821effae1fac4ea4
Gurpreet Saini <sainisaab202@gmail.com>
2024-03-26 14:50:10 -0400

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by GurPreet SaiNi on 2024-03-26.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
